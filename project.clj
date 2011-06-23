0e742d625e9691b6f1e76a4b7fe200f9d5e81562
David Jagoe <david@rheosystems.com>
2011-06-23 11:40:33 +0200

    Comments on dependencies

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
+  ;; TODO upload ltkjava-1.0.0.6-with-dependencies.jar etc to clojars
   :dev-dependencies [[swank-clojure "1.2.1"]])
