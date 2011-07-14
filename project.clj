8328857fdabc13c581caa1328c9dda04c7a4d817
David Jagoe <david@rheosystems.com>
2011-07-14 10:11:10 +0200

    Updated version number.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clipper "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject clipper "0.1.0"
+  :description "RFID reader interface built on LLRP using LLRP Toolkit."
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   ;; TODO upload ltkjava-1.0.0.6-with-dependencies.jar etc to clojars
